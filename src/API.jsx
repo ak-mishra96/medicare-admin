c25a9372896860b6d3a8f72759ad6266ca189078
Atul kumar Mishra <rktatul873@gmail.com>
2025-07-25 18:23:50 +0530

    url

--- src/API.jsx
+++ src/API.jsx
@@ -1,3 +1,3 @@
-export const url = "https://hospital-management-cu00.onrender.com";
+export const url = "https://hospital-backend-lfcw.onrender.com";
 // http://localhost:8080
 // https://hospital-management-cu00.onrender.com
